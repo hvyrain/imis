42bcf051a548bfac17471f2194b706bd8b41f843
Daewoo, Seo <33249369+hvyrain@users.noreply.github.com>
2026-04-08 02:52:37 +0900

    stmain.py 업데이트

--- stmain.py
+++ stmain.py
@@ -1,7 +1,8 @@
 import streamlit as st
-import seaborn as sns
-pg = sns.load_dataset("penguins")
 
 st.title("streamlit 연습장")
 st.header("연습용입니다")
 st.write(sns.pairplot(pg))
+
+import seaborn as sns
+pg = sns.load_dataset("penguins")
